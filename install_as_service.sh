577fbbad093d93ea7cc3c082221cb8de101faa9e
Pliable Pixels <pliablepixels@gmail.com>
2021-01-24 09:47:40 -0500

    same dir check

--- install_as_service.sh
+++ install_as_service.sh
@@ -25,6 +25,17 @@ then
     exit
 fi
 
+# Same dir check
+touch .temp-dir-check.txt 2>/dev/null
+if [ -f ${TARGET_MLAPI_DIR}/.temp-dir-check.txt ]
+then
+    echo "*** Error, the target and source directory seem to be the same! **"
+    rm -f .temp-dir-check.txt 2>/dev/null
+    exit 1
+else
+    rm -f .temp-dir-check.txt 2>/dev/null
+fi
+
 if [ ! -d "${TARGET_MLAPI_DIR}" ]
 then
     echo "Creating ${TARGET_MLAPI_DIR}"
@@ -40,18 +51,6 @@ then
     EXCLUDE_PATTERN="${EXCLUDE_PATTERN} --exclude db"
 fi
 
-if [ -d "${TARGET_MLAPI_DIR}/mlapiconfig.ini" ]
-then
-    echo "Skipping mlapiconfig.ini directory as it already exists in: ${TARGET_MLAPI_DIR}"
-    EXCLUDE_PATTERN="${EXCLUDE_PATTERN} --exclude mlapiconfig.ini"
-fi
-
-if [ -d "${TARGET_MLAPI_DIR}/secrets.ini" ]
-then
-    echo "Skipping secrets.ini directory as it already exists in: ${TARGET_MLAPI_DIR}"
-    EXCLUDE_PATTERN="${EXCLUDE_PATTERN} --exclude secrets.ini"
-fi
-
 if [ -d "${TARGET_MLAPI_DIR}/known_faces" ]
 then
     echo "Skipping known_faces directory as it already exists in: ${TARGET_MLAPI_DIR}"
@@ -64,13 +63,26 @@ then
     EXCLUDE_PATTERN="${EXCLUDE_PATTERN} --exclude unknown_faces"
 fi
 
+if [ -f "${TARGET_MLAPI_DIR}/mlapiconfig.ini" ]
+then
+    echo "Skipping mlapiconfig.ini file as it already exists in: ${TARGET_MLAPI_DIR}"
+    EXCLUDE_PATTERN="${EXCLUDE_PATTERN} --exclude mlapiconfig.ini"
+fi
+
+if [ -f "${TARGET_MLAPI_DIR}/secrets.ini" ]
+then
+    echo "Skipping secrets.ini file as it already exists in: ${TARGET_MLAPI_DIR}"
+    EXCLUDE_PATTERN="${EXCLUDE_PATTERN} --exclude secrets.ini"
+fi
+
+
 echo ${RSYNC} . ${TARGET_MLAPI_DIR} ${EXCLUDE_PATTERN}
 ${RSYNC} . ${TARGET_MLAPI_DIR} ${EXCLUDE_PATTERN}
 
 #cp -R * "${TARGET_MLAPI_DIR}/"
 install -m 755 -o "www-data" -g "www-data" mlapi.py "${TARGET_MLAPI_DIR}" 
 install -m 755 -o "www-data" -g "www-data" mlapi_logrot.sh "${TARGET_MLAPI_DIR}" 
-install -m 755 -o "www-data" -g "www-data" mlapi_face_train.sh "${TARGET_MLAPI_DIR}" 
+install -m 755 -o "www-data" -g "www-data" mlapi_face_train.py "${TARGET_MLAPI_DIR}" 
 
 
 chown -R www-data:www-data ${TARGET_MLAPI_DIR}
